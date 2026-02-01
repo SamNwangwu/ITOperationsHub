22be99d414b7f62279911ba3f4a31d17d3dac66d
Samuel Nwangwu <Samuel.Nwangwu@lebara.com>
2026-02-01 14:20:12 +0000

    Add missing SPFx config files

---
+++ src/webparts/itOpsHomepage/loc/en-us.js
@@ -0,0 +1,9 @@
+define([], function() {
+  return {
+    "PropertyPaneDescription": "IT Operations Homepage Configuration",
+    "BasicGroupName": "Settings",
+    "SiteTypeFieldLabel": "Site Type",
+    "HeroTitleFieldLabel": "Hero Title",
+    "HeroSubtitleFieldLabel": "Hero Subtitle"
+  }
+});
---
+++ src/webparts/itOpsHomepage/loc/mystrings.d.ts
@@ -0,0 +1,12 @@
+declare interface IItOpsHomepageWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  SiteTypeFieldLabel: string;
+  HeroTitleFieldLabel: string;
+  HeroSubtitleFieldLabel: string;
+}
+
+declare module 'ItOpsHomepageWebPartStrings' {
+  const strings: IItOpsHomepageWebPartStrings;
+  export = strings;
+}
